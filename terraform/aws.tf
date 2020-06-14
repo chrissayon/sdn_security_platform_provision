e319390d64f1e5574387c82883bcef16efb0f00f
chrissayon <Christopher.L.Sayon@gmail.com>
2020-06-14 13:18:08 +1000

    Created VPC

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -3,7 +3,20 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "sdnVPC"
+  }
+}
+
+resource "aws_instance" "webEC2" {
   ami           = "ami-03686c686b463366b"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "webserver"
+  }
 }
+
