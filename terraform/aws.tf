df5b34452f1147fbf103df93571bb989c4fae768
chrissayon <Christopher.L.Sayon@gmail.com>
2020-06-14 15:01:29 +1000

    Created security group for web server (frontend)

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -3,6 +3,7 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
+
 resource "aws_vpc" "sdn_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -10,6 +11,49 @@ resource "aws_vpc" "sdn_vpc" {
     Name = "sdnVPC"
   }
 }
+
+
+resource "aws_security_group" "web_security_group" {
+  name        = "web_security_group"
+  description = "Allow ssh and http/https"
+  vpc_id      = aws_vpc.sdn_vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.sdn_vpc.cidr_block]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.sdn_vpc.cidr_block]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.sdn_vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web_security_group"
+  }
+}
+
 
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.sdn_vpc.id
