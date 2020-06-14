4a794e2287926b6351ef3843ff4c887d4762ff79
chrissayon <Christopher.L.Sayon@gmail.com>
2020-06-14 13:59:38 +1000

    Created subnets

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -3,11 +3,31 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "sdn_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "sdnVPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.sdn_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "public_subnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.sdn_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2b"
+
+  tags = {
+    Name = "private_subnet1"
   }
 }
 
